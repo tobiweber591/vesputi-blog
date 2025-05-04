486c80e200bf9101ad6b0b2b7a09bf4cea631a38
Tobias Weber <tobi@DESKTOP-CE1EFR0.>
2025-05-04 17:35:08 +0200

    added known hosts oder so

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Rails.application.configure do
 
   # Only use :id for inspections in production.
   config.active_record.attributes_for_inspect = [ :id ]
-
+  config.hosts << "192.168.178.64"
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
   #   "example.com",     # Allow requests from example.com
